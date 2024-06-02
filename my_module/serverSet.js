e6ebbe0bf62ba2315da3ef4e648b8ed6c77199de
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-06-02 16:55:57 +0900

    POST 부분 함수로 만드려 했으나 역시 실패

--- my_module/serverSet.js
+++ my_module/serverSet.js
@@ -9,6 +9,7 @@ const serverSet = function serverSet(port) {
   const getCurrentDate = require("./timeCheck");
   const getTime = require("./getTime.js");
   const deleteJSON = require("./deleteJSON.js");
+  const searchPOSTTemplate = require("./searchTemplate.js");
   const titleData = require("../public/titleData.json");
   const contentData = require("../public/contentData.json");
   const tagData = require("../public/tagData.json");
@@ -194,129 +195,132 @@ const serverSet = function serverSet(port) {
         res.end();
       });
     }
-    if (req.url === "/searchtitle") {
-      let body = "";
-      req.on("data", (data) => {
-        body += data.toString();
-      });
-      req.on("end", () => {
-        let Jparse = qs.parse(body);
-        let jparse = JSON.stringify(Jparse);
-        let resObj = { title: false };
-        let parseObj = JSON.stringify(resObj);
-        let obj = JSON.parse(parseObj);
-        // console.log(parse.search);
+    searchPOSTTemplate("title");
+    searchPOSTTemplate("content");
+    searchPOSTTemplate("tag");
+    // if (req.url === "/searchtitle") {
+    //   let body = "";
+    //   req.on("data", (data) => {
+    //     body += data.toString();
+    //   });
+    //   req.on("end", () => {
+    //     let Jparse = qs.parse(body);
+    //     let jparse = JSON.stringify(Jparse);
+    //     let resObj = { title: false };
+    //     let parseObj = JSON.stringify(resObj);
+    //     let obj = JSON.parse(parseObj);
+    //     // console.log(parse.search);
 
-        fs.readFile("./public/titleData.json", (err, data) => {
-          let parse = JSON.parse(data);
-          let jArr = [];
-          jArr.push(Jparse.search);
-          console.log(Jparse.search);
-          function templateList(data) {
-            let decode = decodeURI(data);
-            let parse = JSON.parse(decode);
-            let list = "<ul>";
-            for (let i = 0; i < jArr.length; i++) {
-              list =
-                list +
-                `<li><a href="./data/${jArr[i]}.html">${jArr[i]}</a></li>`;
-              `<li>리스트생성</li>`;
-            }
-            list = list + "</ul>";
-            return list;
-          }
-          const htmlList = `${templateList(data)}`;
-          if (parse.includes(Jparse.search)) {
-            res.end(template.searchTemplate(htmlList));
-          } else {
-            res.end("검색 실패");
-          }
-        });
-        // console.log(obj);
-      });
-    }
-    if (req.url === "/searchcontent") {
-      let body = "";
-      req.on("data", (data) => {
-        body += data.toString();
-      });
-      req.on("end", () => {
-        let Jparse = qs.parse(body);
-        let jparse = JSON.stringify(Jparse);
-        let resObj = { title: false };
-        let parseObj = JSON.stringify(resObj);
-        let obj = JSON.parse(parseObj);
-        // console.log(parse.search);
+    //     fs.readFile("./public/titleData.json", (err, data) => {
+    //       let parse = JSON.parse(data);
+    //       let jArr = [];
+    //       jArr.push(Jparse.search);
+    //       console.log(Jparse.search);
+    //       function templateList(data) {
+    //         let decode = decodeURI(data);
+    //         let parse = JSON.parse(decode);
+    //         let list = "<ul>";
+    //         for (let i = 0; i < jArr.length; i++) {
+    //           list =
+    //             list +
+    //             `<li><a href="./data/${jArr[i]}.html">${jArr[i]}</a></li>`;
+    //           `<li>리스트생성</li>`;
+    //         }
+    //         list = list + "</ul>";
+    //         return list;
+    //       }
+    //       const htmlList = `${templateList(data)}`;
+    //       if (parse.includes(Jparse.search)) {
+    //         res.end(template.searchTemplate(htmlList));
+    //       } else {
+    //         res.end("검색 실패");
+    //       }
+    //     });
+    //     // console.log(obj);
+    //   });
+    // }
+    // if (req.url === "/searchcontent") {
+    //   let body = "";
+    //   req.on("data", (data) => {
+    //     body += data.toString();
+    //   });
+    //   req.on("end", () => {
+    //     let Jparse = qs.parse(body);
+    //     let jparse = JSON.stringify(Jparse);
+    //     let resObj = { title: false };
+    //     let parseObj = JSON.stringify(resObj);
+    //     let obj = JSON.parse(parseObj);
+    //     // console.log(parse.search);
 
-        fs.readFile("./public/contentData.json", (err, data) => {
-          let parse = JSON.parse(data);
-          let jArr = [];
-          jArr.push(Jparse.search);
-          console.log(Jparse.search);
-          function templateList(data) {
-            let decode = decodeURI(data);
-            let parse = JSON.parse(decode);
-            let list = "<ul>";
-            for (let i = 0; i < jArr.length; i++) {
-              list =
-                list +
-                `<li><a href="./data/${jArr[i]}.html">${jArr[i]}</a></li>`;
-              `<li>리스트생성</li>`;
-            }
-            list = list + "</ul>";
-            return list;
-          }
-          const htmlList = `${templateList(data)}`;
-          if (parse.includes(Jparse.search)) {
-            res.end(template.searchTemplate(htmlList));
-          } else {
-            res.end("검색 실패");
-          }
-        });
-        // console.log(obj);
-      });
-    }
-    if (req.url === "/searchtag") {
-      let body = "";
-      req.on("data", (data) => {
-        body += data.toString();
-      });
-      req.on("end", () => {
-        let Jparse = qs.parse(body);
-        let jparse = JSON.stringify(Jparse);
-        let resObj = { title: false };
-        let parseObj = JSON.stringify(resObj);
-        let obj = JSON.parse(parseObj);
-        // console.log(parse.search);
+    //     fs.readFile("./public/contentData.json", (err, data) => {
+    //       let parse = JSON.parse(data);
+    //       let jArr = [];
+    //       jArr.push(Jparse.search);
+    //       console.log(Jparse.search);
+    //       function templateList(data) {
+    //         let decode = decodeURI(data);
+    //         let parse = JSON.parse(decode);
+    //         let list = "<ul>";
+    //         for (let i = 0; i < jArr.length; i++) {
+    //           list =
+    //             list +
+    //             `<li><a href="./data/${jArr[i]}.html">${jArr[i]}</a></li>`;
+    //           `<li>리스트생성</li>`;
+    //         }
+    //         list = list + "</ul>";
+    //         return list;
+    //       }
+    //       const htmlList = `${templateList(data)}`;
+    //       if (parse.includes(Jparse.search)) {
+    //         res.end(template.searchTemplate(htmlList));
+    //       } else {
+    //         res.end("검색 실패");
+    //       }
+    //     });
+    //     // console.log(obj);
+    //   });
+    // }
+    // if (req.url === "/searchtag") {
+    //   let body = "";
+    //   req.on("data", (data) => {
+    //     body += data.toString();
+    //   });
+    //   req.on("end", () => {
+    //     let Jparse = qs.parse(body);
+    //     let jparse = JSON.stringify(Jparse);
+    //     let resObj = { title: false };
+    //     let parseObj = JSON.stringify(resObj);
+    //     let obj = JSON.parse(parseObj);
+    //     // console.log(parse.search);
 
-        fs.readFile("./public/tagData.json", (err, data) => {
-          let parse = JSON.parse(data);
-          let jArr = [];
-          jArr.push(Jparse.search);
-          console.log(Jparse.search);
-          function templateList(data) {
-            let decode = decodeURI(data);
-            let parse = JSON.parse(decode);
-            let list = "<ul>";
-            for (let i = 0; i < jArr.length; i++) {
-              list =
-                list +
-                `<li><a href="./data/${jArr[i]}.html">${jArr[i]}</a></li>`;
-              `<li>리스트생성</li>`;
-            }
-            list = list + "</ul>";
-            return list;
-          }
-          const htmlList = `${templateList(data)}`;
-          if (parse.includes(Jparse.search)) {
-            res.end(template.searchTemplate(htmlList));
-          } else {
-            res.end("검색 실패");
-          }
-        });
-        // console.log(obj);
-      });
-    }
+    //     fs.readFile("./public/tagData.json", (err, data) => {
+    //       let parse = JSON.parse(data);
+    //       let jArr = [];
+    //       jArr.push(Jparse.search);
+    //       console.log(Jparse.search);
+    //       function templateList(data) {
+    //         let decode = decodeURI(data);
+    //         let parse = JSON.parse(decode);
+    //         let list = "<ul>";
+    //         for (let i = 0; i < jArr.length; i++) {
+    //           list =
+    //             list +
+    //             `<li><a href="./data/${jArr[i]}.html">${jArr[i]}</a></li>`;
+    //           `<li>리스트생성</li>`;
+    //         }
+    //         list = list + "</ul>";
+    //         return list;
+    //       }
+    //       const htmlList = `${templateList(data)}`;
+    //       if (parse.includes(Jparse.search)) {
+    //         res.end(template.searchTemplate(htmlList));
+    //       } else {
+    //         res.end("검색 실패");
+    //       }
+    //     });
+    //     // console.log(obj);
+    //   });
+    // }
   }
 
   //*서버 생성
